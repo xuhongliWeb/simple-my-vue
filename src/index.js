e9b0c341f52b089f76bdd7f5cc5b50fb0e869b24
xu <564507650@qq.com>
2018-07-06 09:30:46 +0800

    修复热更新

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const add = (a,b) => a + b
 console.log(add(...[1,2]))
-console(222222222)
+console.log('add')
+console.log('ad2222d')
+// 还需要在主要的js文件里写入下面这段代码   module.hot.accept 接受一个回掉 
+// if (module.hot) {
+//     // 实现热更新
+//     module.hot.accept();
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+let webpack = require('webpack')
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -29,13 +30,15 @@ module.exports = {
             // 在src目录下创建一个index.html页面当做模板来用
             template: './src/index.html',
             hash: true, // 会在打包好的bundle.js后面加上hash串
-        })
+        }),
+        // 热更新，热更新不是刷新
+        new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
-        contentBase: './src',
+        contentBase: './dist',
         host: 'localhost',      // 默认是localhost
         port: 3000,             // 端口
-        open: false,             // 自动打开浏览器
-        hot: false               // 开启热更新
+        open: true,             // 自动打开浏览器
+        hot: true               // 开启热更新
     }
 }
